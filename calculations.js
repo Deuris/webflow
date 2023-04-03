4db9f147713b5ac8f79d33ddc9152e200f9d7ee9
Deuris <denys.samofan@gmail.com>
2023-04-03 20:01:51 +0300

    added new code to calculations

--- calculations.js
+++ calculations.js
@@ -207,3 +207,65 @@ noUiSlider.create(anotherslider, {
     anotherslider.noUiSlider.set(this.value);
     ageCalculatorSecond();
   });
+
+
+// First checbox click
+	$(document).ready(function() {
+        $('#checkbox').click(function(event) {
+          var checkBox = document.getElementById("checkbox");
+          if (checkBox.checked == true){
+            $(this).parents('.offers_slider_item_overlay').addClass('active');
+            $(this).parents('.offers_slider_item_overlay').find('.offers_slider_item_range').addClass('active');
+            CheckActive();
+            $(this).parents('.offers_slider_item_overlay').find('.offers_slider_item_toplabel_input').attr("required","required");
+          } else {
+            $('.offers_slider_item_overlay').removeClass('active');
+            $('.offers_slider_item_overlay').find('.offers_slider_item_range').removeClass('active');
+            CheckDisable();
+            $("#checkbox-2").prop( "checked", false );
+            $(this).parents('.offers_slider_item_overlay').find('.offers_slider_item_toplabel_input').removeAttr("required");
+            $("#checkbox-2").parents('.offers_slider_item_overlay').find('.offers_slider_item_toplabel_input').removeAttr("required");
+          }
+          });
+      });
+    
+    // OnInput first slider Input
+    $(document).ready(function() {
+       var checkBox = document.getElementById("checkbox");
+       var slider_input = document.getElementById('slider-range-value');
+            
+       slider_input.oninput = function() {
+          checkBox.checked = true;
+        $(this).parents('.offers_slider_item_overlay').find('.offers_slider_item_range').addClass('active');
+        $(this).parents('.offers_slider_item_overlay').addClass('active');
+        CheckActive();
+        $(this).parents('.offers_slider_item_overlay').find('.offers_slider_item_toplabel_input').attr("required","required");
+      };
+    });
+    // OnInput second slider Input
+    $(document).ready(function() {
+       var checkBox = document.getElementById("checkbox-2");
+       var slider_input = document.getElementById('slider-range-value2');
+        
+       slider_input.oninput = function() {
+          checkBox.checked = true;
+        $(this).parents('.offers_slider_item_overlay').find('.offers_slider_item_range').addClass('active');
+        $(this).parents('.offers_slider_item_overlay').addClass('active');
+        CheckActive();
+        $(this).parents('.offers_slider_item_overlay').find('.offers_slider_item_toplabel_input').attr("required","required");
+      };
+    });
+    function CheckActive() {
+        document.getElementById("checkbox-2").disabled = false;
+    }
+    function CheckDisable() {
+            document.getElementById("checkbox-2").disabled = true;
+    }
+     $(document).ready(function() {
+        CheckDisable()
+    });
+    
+    
+    function ageCalculatorSecond() {
+    
+    }
