2edc408e0b2fb30a0173a0e46c6cb912dccb1d42
Deuris <denys.samofan@gmail.com>
2023-04-04 18:29:12 +0300

    Updated recairtable

---
+++ recairtable cop.js
@@ -0,0 +1,45 @@
+document.getElementById("offers_form").addEventListener("submit", async function (event) {
+    event.preventDefault();
+    await postData();
+  });
+
+  async function postData() {
+    const firstLow = document.getElementById("firstmember_lowPrice").textContent;
+    const firstHigh = document.getElementById("firstmember_hightPrice").textContent;
+    const secondLow = document.getElementById("secondmember_lowPrice").textContent;
+    const secondHigh = document.getElementById("secondmember_hightPrice").textContent;
+    const firstSlider = document.getElementById("slider-range-value").value;
+    const secondSlider = document.getElementById("slider-range-value2").value;
+    const birthDate2 = document.getElementById("offer_secondmember_date").value;
+
+
+    let data = {
+      records: [
+        {
+          fields: {
+            "First Low": firstLow,
+            "First High": firstHigh,
+            "Second Low": secondLow,
+            "Second High": secondHigh,
+            "First Slider": firstSlider,
+            "Second Slider": secondSlider,
+            "Birth date2": birthDate2,
+
+          },
+        },
+      ],
+    };
+
+    await fetch("https://api.airtable.com/v0/appaNukibMwSkbUtp/tblTcv9uJfLLwK1se", {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-type": "application/json",
+        Authorization: "Bearer keyW9wlscQjYRuR2l",
+      },
+      mode: "cors",
+      typecast: true,
+    })
+      .then((response) => response.json())
+      .then((json) => console.log(json));
+  }
--- recairtable.js
+++ recairtable.js
@@ -1,45 +1,47 @@
 document.getElementById("offers_form").addEventListener("submit", async function (event) {
-    event.preventDefault();
-    await postData();
-  });
+  event.preventDefault();
+  await postData();
+});
 
-  async function postData() {
-    const firstLow = document.getElementById("firstmember_lowPrice").textContent;
-    const firstHigh = document.getElementById("firstmember_hightPrice").textContent;
-    const secondLow = document.getElementById("secondmember_lowPrice").textContent;
-    const secondHigh = document.getElementById("secondmember_hightPrice").textContent;
-    const firstSlider = document.getElementById("slider-range-value").value;
-    const secondSlider = document.getElementById("slider-range-value2").value;
-    const birthDate2 = document.getElementById("offer_secondmember_date").value;
+async function postData() {
+  const firstLow = document.getElementById("firstmember_lowPrice").textContent;
+  const firstHigh = document.getElementById("firstmember_hightPrice").textContent;
+  const secondLow = document.getElementById("secondmember_lowPrice").textContent;
+  const secondHigh = document.getElementById("secondmember_hightPrice").textContent;
+  const firstSlider = document.getElementById("slider-range-value").value;
+  const secondSlider = document.getElementById("slider-range-value2").value;
+  const birthDate2 = document.getElementById("offer_secondmember_date").value;
+  const checkbox2 = document.getElementById("checkbox-2");
 
-
-    let data = {
-      records: [
-        {
-          fields: {
-            "First Low": firstLow,
-            "First High": firstHigh,
-            "Second Low": secondLow,
-            "Second High": secondHigh,
-            "First Slider": firstSlider,
-            "Second Slider": secondSlider,
-            "Birth date2": birthDate2,
-
-          },
+  let data = {
+    records: [
+      {
+        fields: {
+          "First Low": firstLow,
+          "First High": firstHigh,
+          "Second High": secondHigh,
+          "First Slider": firstSlider,
+          "Second Slider": secondSlider,
+          "Birth date2": birthDate2,
         },
-      ],
-    };
-
-    await fetch("https://api.airtable.com/v0/appaNukibMwSkbUtp/tblTcv9uJfLLwK1se", {
-      method: "POST",
-      body: JSON.stringify(data),
-      headers: {
-        "Content-type": "application/json",
-        Authorization: "Bearer keyW9wlscQjYRuR2l",
       },
-      mode: "cors",
-      typecast: true,
-    })
-      .then((response) => response.json())
-      .then((json) => console.log(json));
+    ],
+  };
+
+  if (checkbox2.checked) {
+    data.records[0].fields["Second Low"] = secondLow;
   }
+
+  await fetch("https://api.airtable.com/v0/appaNukibMwSkbUtp/tblTcv9uJfLLwK1se", {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-type": "application/json",
+      Authorization: "Bearer keyW9wlscQjYRuR2l",
+    },
+    mode: "cors",
+    typecast: true,
+  })
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+}
